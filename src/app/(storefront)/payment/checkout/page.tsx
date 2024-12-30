9bc211756ea050162dbc824fa6fa11312bbb16b6
Nikojuu <come_on12345@hotmail.com>
2024-12-30 23:11:55 +0200

    stripe 2

--- src/app/(storefront)/payment/checkout/page.tsx
+++ src/app/(storefront)/payment/checkout/page.tsx
@@ -1,23 +1,25 @@
-// import PaytrailCheckoutPage from "@/components/Checkout/PaytrailCheckoutPage";
-// import CheckoutPage from "@/components/Checkout/PaytrailCheckoutPage";
-// import PaytrailCheckout from "@/components/Checkout/PaytrailPayments";
-// import { Metadata } from "next";
+import PaytrailCheckoutPage from "@/components/Checkout/PaytrailCheckoutPage";
+import CheckoutPage from "@/components/Checkout/PaytrailCheckoutPage";
+import PaytrailCheckout from "@/components/Checkout/PaytrailPayments";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
-// export const metadata: Metadata = {
-//   title: "Pupun Korvat | Tilaus",
-//   description: "Tilaa käsintehtyjä koruja Pupun Korvista.",
-//   robots: {
-//     index: false,
-//     follow: false,
-//   },
-//   openGraph: {
-//     title: "Pupun Korvat |  Tilaus",
-//     type: "website",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Pupun Korvat | Tilaus",
+  description: "Tilaa käsintehtyjä koruja Pupun Korvista.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+  openGraph: {
+    title: "Pupun Korvat |  Tilaus",
+    type: "website",
+  },
+};
 
-// const CheckoutRoute = () => {
-//   return <PaytrailCheckoutPage />;
-// };
+const CheckoutRoute = () => {
+  return notFound();
+  // return <PaytrailCheckoutPage />;
+};
 
-// export default CheckoutRoute;
+export default CheckoutRoute;
