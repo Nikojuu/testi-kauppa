c65d81fd3a83a585b702a7e70fb40190f028489b
Nikojuu <come_on12345@hotmail.com>
2025-02-23 00:09:41 +0200

    removed database 3

--- src/app/(storefront)/payment/checkout/cancel-route/route.ts
+++ src/app/(storefront)/payment/checkout/cancel-route/route.ts
@@ -1,36 +1,36 @@
-// import { calculateHmac } from "@/app/utils/calculateHmac";
+import { calculateHmac } from "@/app/utils/calculateHmac";
 
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function GET(request: Request) {
-//   const { searchParams } = new URL(request.url);
-//   const checkoutParams: Record<string, string> = {};
-//   searchParams.forEach((value, key) => {
-//     if (key.startsWith("checkout-")) {
-//       checkoutParams[key] = value;
-//     }
-//   });
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const checkoutParams: Record<string, string> = {};
+  searchParams.forEach((value, key) => {
+    if (key.startsWith("checkout-")) {
+      checkoutParams[key] = value;
+    }
+  });
 
-//   const receivedSignature = searchParams.get("signature");
-//   const calculatedSignature = calculateHmac(
-//     process.env.PAYTRAIL_MERCHANT_SECRET!,
-//     checkoutParams,
-//     null
-//   );
+  const receivedSignature = searchParams.get("signature");
+  const calculatedSignature = calculateHmac(
+    process.env.PAYTRAIL_MERCHANT_SECRET!,
+    checkoutParams,
+    null
+  );
 
-//   if (receivedSignature !== calculatedSignature) {
-//     return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
-//   }
+  if (receivedSignature !== calculatedSignature) {
+    return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
+  }
 
-//   const reference = checkoutParams["checkout-reference"];
+  const reference = checkoutParams["checkout-reference"];
 
-//   try {
-//     await prisma.order.delete({
-//       where: { id: reference, storeId: process.env.TENANT_ID },
-//     });
+  try {
+    // await prisma.order.delete({
+    //   where: { id: reference, storeId: process.env.TENANT_ID },
+    // });
 
-//     return NextResponse.redirect(new URL("/payment/cancel", request.url));
-//   } catch (error) {
-//     console.error("Error Deleting from order from database", error);
-//   }
-// }
+    return NextResponse.redirect(new URL("/payment/cancel", request.url));
+  } catch (error) {
+    console.error("Error Deleting from order from database", error);
+  }
+}
