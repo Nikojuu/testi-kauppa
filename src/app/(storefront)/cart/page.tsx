0608cc84c723ce048d6e13be11c61208f3b15362
Nikojuu <come_on12345@hotmail.com>
2025-07-19 12:20:40 +0300

    carpage commentedout

--- src/app/(storefront)/cart/page.tsx
+++ src/app/(storefront)/cart/page.tsx
@@ -1,15 +1,16 @@
 // import CartPage from "@/components/Cart/CartPage";
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Pupun Korvat | Ostoskori",
-//   description:
-//     "Tutustu Pupun Korvien käsintehtyihin koruihin ja löydä ainutlaatuinen lahja tai itsellesi sopiva koru.",
-//   robots: "noindex, nofollow",
-// };
+export const metadata: Metadata = {
+  title: "Pupun Korvat | Ostoskori",
+  description:
+    "Tutustu Pupun Korvien käsintehtyihin koruihin ja löydä ainutlaatuinen lahja tai itsellesi sopiva koru.",
+  robots: "noindex, nofollow",
+};
 
-// const CartRoute = () => {
-//   return <CartPage />;
-// };
+const CartRoute = () => {
+  // return <CartPage />;
+  return <h1>Cart</h1>;
+};
 
-// export default CartRoute;
+export default CartRoute;
