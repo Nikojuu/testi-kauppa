105d520e06008bb8b1c3a902582e400dbcf9bd76
Nikojuu <come_on12345@hotmail.com>
2025-08-21 23:45:37 +0300

    shipiment logic moved to endpoint

--- src/lib/actions/shipmentActions.ts
+++ src/lib/actions/shipmentActions.ts
@@ -11,7 +11,7 @@ export async function getShipmentMethods(
 ): Promise<ShipmentMethodsWithLocations> {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_STOREFRONT_API_URL}/api/storefront/v1/shipment-methods`,
+      `${process.env.NEXT_PUBLIC_STOREFRONT_API_URL}/api/storefront/v1/shipment-methods/${postalCode}`,
       {
         method: "GET",
         headers: {
@@ -27,146 +27,86 @@ export async function getShipmentMethods(
       );
     }
 
-    const apiResponse: ApiResponseShipmentMethods = await res.json();
-
-    const serviceIds = apiResponse.shipmentMethods
-      .map((method) => method.shipitMethod?.serviceId)
-      .filter((id): id is string => id !== undefined && id !== null);
-
-    if (serviceIds.length === 0) {
-      console.log("No service IDs found, skipping Shipit API call");
-      return {
-        pricedLocations: [],
-        shipmentMethods: apiResponse.shipmentMethods,
-      };
-    }
-
-    const requestBody = {
-      postcode: postalCode,
-      country: "FI",
-      serviceId: serviceIds,
-      type: "parcel_locker",
-      limit: 20,
-    };
-
-    const response = await fetch(process.env.SHIPIT_API_URL! + "/agents", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-SHIPIT-KEY": process.env.SHIPIT_API_KEY!,
-      },
-      body: JSON.stringify(requestBody),
-    });
-
-    if (!response.ok) {
-      throw new Error(`API call failed with status: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    if (!data.locations) {
-      console.log("No locations found");
-      return {
-        pricedLocations: [],
-        shipmentMethods: apiResponse.shipmentMethods,
-      };
-    }
-
-    const pricedLocations = data.locations.map(
-      (location: ShipitAgentLocation) => {
-        const matchingMethod = apiResponse.shipmentMethods.find(
-          (method) => method.shipitMethod?.serviceId === location.serviceId
-        );
-        return {
-          ...location,
-          merchantPrice: matchingMethod ? matchingMethod.price : null,
-        };
-      }
-    );
-
-    return {
-      pricedLocations,
-      shipmentMethods: apiResponse.shipmentMethods,
-    };
+    return await res.json();
   } catch (error) {
     console.error("Error fetching shipment methods:", error);
     throw error;
   }
 }
 
-export const getDropInLocations = async ({
-  customerPostalCode,
-}: {
-  customerPostalCode: string;
-}) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_STOREFRONT_API_URL}/api/storefront/v1/shipment-methods`,
-    {
-      method: "GET",
-      headers: {
-        "x-api-key": process.env.STOREFRONT_API_KEY || "",
-      },
-    }
-  );
-  const shipmentMethods: ApiResponseShipmentMethods = await res.json();
-  const shipitShipmentMethods = shipmentMethods.shipmentMethods.map(
-    (method) => method.shipitMethod
-  );
+// export const getDropInLocations = async ({
+//   customerPostalCode,
+// }: {
+//   customerPostalCode: string;
+// }) => {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_STOREFRONT_API_URL}/api/storefront/v1/shipment-methods`,
+//     {
+//       method: "GET",
+//       headers: {
+//         "x-api-key": process.env.STOREFRONT_API_KEY || "",
+//       },
+//     }
+//   );
+//   const shipmentMethods: ApiResponseShipmentMethods = await res.json();
+//   const shipitShipmentMethods = shipmentMethods.shipmentMethods.map(
+//     (method) => method.shipitMethod
+//   );
 
-  if (shipitShipmentMethods.length === 0) {
-    return [];
-  }
+//   if (shipitShipmentMethods.length === 0) {
+//     return [];
+//   }
 
-  // const url =
-  //   process.env.NODE_ENV === "development"
-  //     ? "https://apitest.shipit.ax/v1/agents"
-  //     : "https://api.shipit.ax/v1/agents";
-  const url = process.env.SHIPIT_API_URL!; // Use the environment variable for the URL
+//   // const url =
+//   //   process.env.NODE_ENV === "development"
+//   //     ? "https://apitest.shipit.ax/v1/agents"
+//   //     : "https://api.shipit.ax/v1/agents";
+//   const url = process.env.SHIPIT_API_URL!; // Use the environment variable for the URL
 
-  const requestBody = {
-    postcode: customerPostalCode,
-    country: "FI",
-    serviceId: shipitShipmentMethods.map((method) => method.serviceId),
-    type: "PICKUP",
-  };
+//   const requestBody = {
+//     postcode: customerPostalCode,
+//     country: "FI",
+//     serviceId: shipitShipmentMethods.map((method) => method.serviceId),
+//     type: "PICKUP",
+//   };
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-SHIPIT-KEY": process.env.SHIPIT_API_KEY!,
-      },
-      body: JSON.stringify(requestBody),
-    });
+//   try {
+//     const response = await fetch(url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "X-SHIPIT-KEY": process.env.SHIPIT_API_KEY!,
+//       },
+//       body: JSON.stringify(requestBody),
+//     });
 
-    if (!response.ok) {
-      throw new Error(`API call failed with status: ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`API call failed with status: ${response.status}`);
+//     }
 
-    const data = await response.json();
-    if (!data.locations) {
-      console.log("No locations found");
-      return [];
-    }
-    // this basically just adds the price from database to the location returned from the API
-    const enrichedData = data.locations
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .map((location: any) => {
-        const matchingMethod = shipitShipmentMethods.find(
-          (method) => method.serviceId === location.serviceId
-        );
+//     const data = await response.json();
+//     if (!data.locations) {
+//       console.log("No locations found");
+//       return [];
+//     }
+//     // this basically just adds the price from database to the location returned from the API
+//     const enrichedData = data.locations
+//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//       .map((location: any) => {
+//         const matchingMethod = shipitShipmentMethods.find(
+//           (method) => method.serviceId === location.serviceId
+//         );
 
-        return {
-          ...location,
-          merchantPrice: matchingMethod ? matchingMethod.price : null,
-        };
-      })
-      .slice(0, 20);
+//         return {
+//           ...location,
+//           merchantPrice: matchingMethod ? matchingMethod.price : null,
+//         };
+//       })
+//       .slice(0, 20);
 
-    return enrichedData; // Return the enriched data
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error; // Optionally rethrow to handle it in the calling function
-  }
-};
+//     return enrichedData; // Return the enriched data
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw error; // Optionally rethrow to handle it in the calling function
+//   }
+// };
